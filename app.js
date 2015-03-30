35c4690ad68820188f9688cd4162b30064740a8f
Nils Schneider <nils@nilsschneider.net>
2015-03-30 02:58:47 +0200

    statistics

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require.config({
     "moment": "../bower_components/moment/min/moment-with-locales.min",
     "tablesort": "../bower_components/tablesort/tablesort.min",
     "tablesort.numeric": "../bower_components/tablesort/src/sorts/tablesort.numeric",
+    "chartjs": "../bower_components/chartjs/Chart",
     "helper": "../helper"
   },
   shim: {
