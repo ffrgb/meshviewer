26368ca337b8bd78fb7807a2763ebed5f3769b64
Nils Schneider <nils@nilsschneider.net>
2016-02-29 14:34:37 +0100

    [TASK] remove build.js, it is not needed anymore

--- build.js
+++
@@ -1,9 +0,0 @@
-({
-  baseUrl: "lib",
-  name: "../bower_components/almond/almond",
-  mainConfigFile: "app.js",
-  include: "../app",
-  wrap: true,
-  optimize: "uglify",
-  out: "app-combined.js"
-});
