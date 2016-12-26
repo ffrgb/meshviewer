bcd126464f505fa61d2390394ff1642761d734e2
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2016-12-26 03:44:59 +0100

    [TASK] Update to moment.js 2.17 & several small bugfix updates

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ require.config({
     "leaflet": "../bower_components/leaflet/dist/leaflet",
     "leaflet.label": "../bower_components/Leaflet.label/dist/leaflet.label",
     "chroma-js": "../bower_components/chroma-js/chroma.min",
-    "moment": "../bower_components/moment/min/moment.min",
-    "moment.de": "../bower_components/moment/locale/de",
+    "moment": "../bower_components/moment",
     "tablesort": "../bower_components/tablesort/src/tablesort",
     "d3": "../bower_components/d3/d3.min",
     "virtual-dom": "../bower_components/virtual-dom/dist/virtual-dom",
@@ -16,7 +15,6 @@ require.config({
   },
   shim: {
     "leaflet.label": ["leaflet"],
-    "moment.de": ["moment"],
     "tablesort": {
       exports: "Tablesort"
     }
