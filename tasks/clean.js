4e959b14d030beb84adaddd4837c7067a656b00a
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2017-01-10 23:38:57 +0100

    [TASK] Improve *.map cleanup

--- tasks/clean.js
+++ tasks/clean.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
   grunt.config.merge({
     clean: {
       build: ["build/**/*", "node_modules/grunt-newer/.cache"],
-      release: ["build/vendor", "build/app.js.map", "build/config.json", "build/style.css*"]
+      release: ["build/vendor", "build/*.map", "build/config.json", "build/style.css"]
     }
   });
 
