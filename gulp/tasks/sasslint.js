0a2f171f22a9799ec5f303b6347f855c85b7253f
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2017-04-14 14:56:44 +0200

    [BUGFIX] Sasslint file matching

--- gulp/tasks/sasslint.js
+++ gulp/tasks/sasslint.js
@@ -1,6 +1,6 @@
 module.exports = function (gulp, plugins, config, env) {
   return function sasslint() {
-    return gulp.src('scss/*.scss')
+    return gulp.src('scss/**/*.scss')
       .pipe(plugins.sassLint())
       .pipe(plugins.sassLint.format())
       .pipe(env.production(plugins.sassLint.failOnError()));
