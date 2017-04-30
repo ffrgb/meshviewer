ebc3ca4804e32257670243372acc9b8930bae30c
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2017-04-30 14:59:39 +0200

    [TASK] Move polyfills and test properties

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -4,7 +4,7 @@ module.exports = function (gulp, plugins, config) {
       .pipe(gulp.dest(config.build));
     gulp.src(['assets/logo.svg'])
       .pipe(gulp.dest(config.build));
-    gulp.src('node_modules/promise-polyfill/promise.js')
+    gulp.src(['node_modules/promise-polyfill/promise.js', 'polyfill.js'])
       .pipe(gulp.dest(config.build + '/vendor'));
     return gulp.src(['assets/fonts/*', 'assets/icons/fonts/*'])
       .pipe(gulp.dest(config.build + '/fonts'));
---
+++ polyfill.js
@@ -0,0 +1,80 @@
+'use strict';
+// Polyfills for (old) mobile browsers and IE 11
+// From https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/
+if (!String.prototype.includes) {
+  String.prototype.includes = function () {
+    return String.prototype.indexOf.apply(this, arguments) !== -1;
+  };
+}
+
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function (searchString, position) {
+    position = position || 0;
+    return this.substr(position, searchString.length) === searchString;
+  };
+}
+
+if (!String.prototype.repeat) {
+  String.prototype.repeat = function (count) {
+    'use strict';
+    if (this === null) {
+      throw new TypeError('can\'t convert ' + this + ' to object');
+    }
+    var str = '' + this;
+    count = +count;
+    if (count < 0) {
+      throw new RangeError('repeat count must be non-negative');
+    }
+    if (count === Infinity) {
+      throw new RangeError('repeat count must be less than infinity');
+    }
+    count = Math.floor(count);
+    if (str.length === 0 || count === 0) {
+      return '';
+    }
+    // Ensuring count is a 31-bit integer allows us to heavily optimize the
+    // main part. But anyway, most current (August 2014) browsers can't handle
+    // strings 1 << 28 chars or longer, so:
+    if (str.length * count >= 1 << 28) {
+      throw new RangeError('repeat count must not overflow maximum string size');
+    }
+    var rpt = '';
+    for (; ;) {
+      if ((count & 1) === 1) {
+        rpt += str;
+      }
+      count >>>= 1;
+      if (count === 0) {
+        break;
+      }
+      str += str;
+    }
+    // Could we try:
+    // return Array(count + 1).join(this);
+    return rpt;
+  };
+}
+
+if (typeof Object.assign !== 'function') {
+  Object.assign = function(target, varArgs) { // .length of function is 2
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        for (var nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
