8df5c2ce8063273bdd2a69ae0562b6c890158cf0
Xaver Maierhofer <xmaierhofer@1drop.de>
2017-05-24 16:15:50 +0200

    [TASK] Upgrade to uglify3 and minor lib updates

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -3,7 +3,7 @@ module.exports = function (gulp, plugins, config, env) {
     return gulp.src('app.js')
       .pipe(env.development(plugins.sourcemaps.init()))
       .pipe(plugins.requirejsOptimize(env.production() ? config.requireJs.prod : config.requireJs.dev))
-      .pipe(env.production(plugins.uglify({ preserveComments: 'license' })))
+      .pipe(env.production(plugins.uglify({ output: { comments: 'all' } })))
       .pipe(env.development(plugins.sourcemaps.write('.', { addComment: true })))
       .pipe(gulp.dest(config.build));
   };
