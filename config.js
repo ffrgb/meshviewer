0efdca09e6d234f38899f9ab3f6b0bd2e79d8056
Martin Geno <geno+dev@fireorbit.de>
2017-11-02 22:16:38 +0100

    [TASK] Add multiple communities map

--- config.js
+++ config.js
@@ -1,33 +1,9 @@
 module.exports = function () {
   return {
-    // Variables are NODE_ID and NODE_NAME (only a-z0-9\- other chars are replaced with _)
-    'nodeInfos': [
-      {
-        'name': 'Clientstatistik',
-        'href': 'https://regensburg.freifunk.net/netz/statistik/node/{NODE_ID}/',
-        'image': 'https://grafana.regensburg.freifunk.net/render/dashboard-solo/db/ffrgb-all-nodes?panelId=1&from=now-7d&var-nodeid={NODE_ID}&var-host={NODE_NAME}&width=650&height=350&theme=light&_t={TIME}',
-        'title': 'Knoten {NODE_ID} - weiteren Statistiken'
-      },
-      {
-        'name': 'Trafficstatistik',
-        'href': 'https://regensburg.freifunk.net/netz/statistik/node/{NODE_ID}/',
-        'image': 'https://grafana.regensburg.freifunk.net/render/dashboard-solo/db/ffrgb-all-nodes?panelId=2&from=now-7d&var-nodeid={NODE_ID}&var-host={NODE_NAME}&width=650&height=350&theme=light&_t={TIME}',
-        'title': 'Knoten {NODE_ID} - weiteren Statistiken'
-      }
-    ],
-    'globalInfos': [
-      {
-        'name': 'Statistik',
-        'href': 'https://regensburg.freifunk.net/netz/statistik/',
-        'image': 'https://grafana.regensburg.freifunk.net/render/dashboard-solo/db/ffrgb-network-wide-stats?panelId=11&from=now-1y&width=600&height=350&theme=light',
-        'title': 'Jahresstatistik - weiteren Statistiken'
-      }
-    ],
+    'maxAge': 2,
     // Array of data provider are supported
-    'dataPath': [
-      'https://regensburg.freifunk.net/data/'
-    ],
-    'siteName': 'Freifunk Regensburg',
+    'dataPath': ['https://web.fireorbit.de/meshviewer/data/'],
+    'siteName': 'Freifunk Multiple Communities',
     'mapLayers': [
       {
         'name': 'Freifunk Regensburg',
@@ -36,21 +12,7 @@ module.exports = function () {
         'config': {
           'maxZoom': 20,
           'subdomains': '1234',
-          'attribution': '<a href="http://www.openmaptiles.org/" target="_blank">&copy; OpenMapTiles</a> <a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap contributors</a>',
-          'start': 6
-        }
-      },
-      {
-        'name': 'Freifunk Regensburg Night',
-        // Please ask Freifunk Regensburg before using its tile server - example with retina and dark tiles
-        'url': 'https://{s}.tiles.ffrgb.net/n/{z}/{x}/{y}{retina}.png',
-        'config': {
-          'maxZoom': 20,
-          'subdomains': '1234',
-          'attribution': ' <a href="http://www.openmaptiles.org/" target="_blank">&copy; OpenMapTiles</a> <a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap contributors</a>',
-          'mode': 'night',
-          'start': 19,
-          'end': 7
+          'attribution': '<a href="http://www.openmaptiles.org/" target="_blank">&copy; OpenMapTiles</a> <a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap contributors</a>'
         }
       },
       {
@@ -62,32 +24,11 @@ module.exports = function () {
         }
       },
       {
-        'name': 'HERE',
-        // Please use your own API key - Free plan is on right side after the pay plans
-        'url': 'https://{s}.base.maps.api.here.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8?app_id=YOUR_KEY&app_code=YOUR_CODE&lg=deu',
-        'config': {
-          'attribution': 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
-          'subdomains': '1234',
-          'maxZoom': 20
-        }
-      },
-      {
         'name': 'Esri.WorldImagery',
         'url': '//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
         'config': {
           'maxZoom': 20,
           'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-        }
-      },
-      {
-        'name': 'HERE.hybridDay',
-        // Please use your own API key - Free plan is on right side after the pay plans
-        'url': 'https://{s}.aerial.maps.api.here.com/maptile/2.1/maptile/newest/{variant}/{z}/{x}/{y}/256/png8?app_id=YOUR_KEY&app_code=YOUR_CODE&lg=deu',
-        'config': {
-          'attribution': 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
-          'subdomains': '1234',
-          'variant': 'hybrid.day',
-          'maxZoom': 20
         }
       }
     ],
@@ -95,25 +36,77 @@ module.exports = function () {
     'fixedCenter': [
       // Northwest
       [
-        49.3522,
-        11.7752
+        54.8449,
+        6.07543
       ],
       // Southeast
       [
-        48.7480,
-        12.8917
+        47.8721,
+        13.9526
       ]
     ],
     'siteNames': [
       {
-        'site': 'ffrgb-bat15',
-        'name': 'Regensburg'
-      },
-      {
+        'site': 'ffac',
+        'name': 'Aachen'
+      }, {
+        'site': 'ffhb',
+        'name': 'Bremen'
+      }, {
+        'site': 'ffda',
+        'name': 'Darmstadt'
+      }, {
+        'site': 'ffdef',
+        'name': 'Darmstadt'
+      }, {
+        'site': 'ffein',
+        'name': 'Einbeck'
+      }, {
+        'site': 'ffen',
+        'name': 'Ennepe-Ruhr-Kreis (EN-Kreis)'
+      }, {
+        'site': 'ffept',
+        'name': 'Ennepe-Ruhr-Kreis (Ennepetal)'
+      }, {
+        'site': 'ffhat',
+        'name': 'Ennepe-Ruhr-Kreis (Hattingen)'
+      }, {
+        'site': 'ffspr',
+        'name': 'Ennepe-Ruhr-Kreis (Sprockhoevel)'
+      }, {
+        'site': 'ffwtt',
+        'name': 'Ennepe-Ruhr-Kreis (Wetter)'
+      }, {
+        'site': 'ffwit',
+        'name': 'Ennepe-Ruhr-Kreis (Witten)'
+      }, {
+        'site': 'ffdus',
+        'name': 'Flingern'
+      }, {
+        'site': 'ffffm',
+        'name': 'Frankfurt'
+      }, {
+        'site': 'ffggrz',
+        'name': 'Gera-Greiz'
+      }, {
+        'site': 'ffis',
+        'name': 'Iserlohn'
+      }, {
+        'site': 'ffki',
+        'name': 'Kiel'
+      }, {
         'site': 'ffrgb',
         'name': 'Regensburg'
+      }, {
+        'site': 'ffrgb-bat15',
+        'name': 'Regensburg'
+      }, {
+        'site': 'fftr',
+        'name': 'Trier'
+      }, {
+        'site': 'ffwp',
+        'name': 'Westpfalz'
       }
     ]
   };
 };
-
