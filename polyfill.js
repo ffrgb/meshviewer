6e35ace89d9a238e2a96b4d9c9abb58a638a108e
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2020-05-31 10:51:50 +0200

    Remove IE polyfills

--- polyfill.js
+++ polyfill.js
@@ -1,12 +1,6 @@
 'use strict';
-// Polyfills for (old) mobile browsers and IE 11
+// Polyfills for (old) firefox 75
 // From https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/
-if (!String.prototype.includes) {
-  String.prototype.includes = function () {
-    return String.prototype.indexOf.apply(this, arguments) !== -1;
-  };
-}
-
 if (typeof Object.assign !== 'function') {
   Object.assign = function (target, varArgs) { // .length of function is 2
     if (target == null) { // TypeError if undefined or null
@@ -30,24 +24,6 @@ if (typeof Object.assign !== 'function') {
     return to;
   };
 }
-
-// eslint-disable-next-line consistent-return
-(function () {
-  if (typeof window.CustomEvent === 'function') {
-    return false;
-  }
-
-  function CustomEvent(event, params) {
-    params = params || { bubbles: false, cancelable: false, detail: undefined };
-    var evt = document.createEvent('CustomEvent');
-    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-    return evt;
-  }
-
-  CustomEvent.prototype = window.Event.prototype;
-
-  window.CustomEvent = CustomEvent;
-})();
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js');
