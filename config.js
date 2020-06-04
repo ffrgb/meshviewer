c601fc93d1ca1e0d1a25659f5d7b0c6a9828f416
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2020-06-04 19:11:14 +0200

    Add ratio & alt attribute to stats images

--- config.js
+++ config.js
@@ -6,32 +6,42 @@ module.exports = function () {
         'name': 'Clientstatistik',
         'href': 'https://regensburg.freifunk.net/netz/statistik/node/{NODE_ID}/',
         'image': 'https://grafana.regensburg.freifunk.net/render/d-solo/000000026/node?panelId=1&var-node={NODE_ID}&from=now-1d&width=650&height=350&theme=light&_t={TIME}',
-        'title': 'Clientstatistik für {NODE_ID} - weiteren Statistiken'
+        'title': 'Clientstatistik für {NODE_ID} - weiteren Statistiken',
+        'width': 650,
+        'height': 350
       },
       {
         'name': 'Trafficstatistik',
         'href': 'https://regensburg.freifunk.net/netz/statistik/node/{NODE_ID}/',
         'image': 'https://grafana.regensburg.freifunk.net/render/d-solo/000000026/node?panelId=2&from=now-1d&var-node={NODE_ID}&width=650&height=350&theme=light&_t={TIME}',
-        'title': 'Trafficstatistik für {NODE_ID} - weiteren Statistiken'
+        'title': 'Trafficstatistik für {NODE_ID} - weiteren Statistiken',
+        'width': 650,
+        'height': 350
       },
       {
         'name': 'Systemlast',
         'href': 'https://regensburg.freifunk.net/netz/statistik/node/{NODE_ID}/',
         'image': 'https://grafana.regensburg.freifunk.net/render/d-solo/000000026/node?panelId=4&from=now-1d&var-node={NODE_ID}&width=650&height=350&theme=light&_t={TIME}',
-        'title': 'Systemlast für {NODE_ID} - weiteren Statistiken'
+        'title': 'Systemlast für {NODE_ID} - weiteren Statistiken',
+        'width': 650,
+        'height': 350
       },
       {
         'name': 'Airtime',
         'href': 'https://regensburg.freifunk.net/netz/statistik/node/{NODE_ID}/',
         'image': 'https://grafana.regensburg.freifunk.net/render/d-solo/000000026/node?panelId=5&from=now-1d&var-node={NODE_ID}&width=650&height=350&theme=light&_t={TIME}',
-        'title': 'Airtime für {NODE_ID} - weiteren Statistiken'
+        'title': 'Airtime für {NODE_ID} - weiteren Statistiken',
+        'width': 650,
+        'height': 350
       }
     ],
     'linkInfos': [
       {
         'name': 'Statistik für alle Links zwischen diese Knoten',
         'image': 'https://grafana.regensburg.freifunk.net/render/d-solo/nvSNqoHmz/link?panelId=7&var-node={SOURCE_ID}&var-nodetolink={TARGET_ID}&from=now-1d&&width=650&height=350&theme=light&_t={TIME}',
-        'title': 'Linkstatistik des letzten Tages, min und max aller Links zwischen diesen Knoten'
+        'title': 'Linkstatistik des letzten Tages, min und max aller Links zwischen diesen Knoten',
+        'width': 650,
+        'height': 350
       }
     ],
     'globalInfos': [
@@ -39,7 +49,9 @@ module.exports = function () {
         'name': 'Globale Statistik',
         'href': 'https://regensburg.freifunk.net/netz/statistik',
         'image': 'https://grafana.regensburg.freifunk.net/render/d-solo/000000028/globals?panelId=2&from=now-7d&&width=650&height=350&theme=light&_t={TIME}',
-        'title': 'Globale Statistik - weiteren Statistiken'
+        'title': 'Globale Statistik - weiteren Statistiken',
+        'width': 650,
+        'height': 350
       }
     ],
     // Array of data provider are supported
